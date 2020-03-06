51c43b75dc19cc2a41e6f2088d73b6d0388a51ad
Abdullah Althobetey <abdullah.th@me.com>
2020-03-06 18:46:06 +0300

    Add instance variables to NearEarthObject and OrbitPath

--- starter/models.py
+++ starter/models.py
@@ -10,6 +10,8 @@ class NearEarthObject(object):
         :param kwargs:    dict of attributes about a given Near Earth Object, only a subset of attributes used
         """
         # TODO: What instance variables will be useful for storing on the Near Earth Object?
+        self.attributes = kwargs
+        self.orbits = []
 
     def update_orbits(self, orbit):
         """
@@ -20,6 +22,7 @@ class NearEarthObject(object):
         """
 
         # TODO: How do we connect orbits back to the Near Earth Object?
+        self.orbits.append(orbit)
 
 
 class OrbitPath(object):
@@ -34,3 +37,5 @@ class OrbitPath(object):
         :param kwargs:    dict of attributes about a given orbit, only a subset of attributes used
         """
         # TODO: What instance variables will be useful for storing on the Near Earth Object?
+        self.neo = NearEarthObject()
+        self.attributes = kwargs
